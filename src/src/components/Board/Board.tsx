224372c1222f8acd94292b4a9be64c8855671024
bumi <108353289+bumiterra@users.noreply.github.com>
2023-08-04 15:16:09 +0700

    feat:board

---
+++ src/src/components/Board/Board.tsx
@@ -0,0 +1,12 @@
+const Board = () => {
+    return (
+        <>
+            <div className="w-[387px] h-[468px] bg-[#F7F1E5] border-[10px] border-solid border-[#E7B10A] rounded-[100px] flex items-center justify-center">
+                <h2 className="font-[400] text-[64px] leading-[80.06px]">Board 1</h2>
+            </div>
+        </>
+    )
+
+}
+
+export default Board
