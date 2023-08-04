46bfce12486bb41f9191e5d52ea16bb76bfdcb14
Harryts1 <harrytrumansuhalim8@gmail.com>
2023-08-04 12:29:01 +0700

    feat : dashboard-admin

--- src/src/app/dashboard-admin/page.tsx
+++ src/src/app/dashboard-admin/page.tsx
@@ -5,6 +5,11 @@ const DashboardAdmin = () => {
   return (
     <>
         <HeaderAdmin />
+        <div className="min-h-screen h-max min-w-screen">
+          <div className="h-[400px] w-[400px] bg-[#E7B10A] rounded-br-full">
+            <h2 className="h-auto w-[337px] text-[64px] p-6">Welcome, User123!</h2>
+          </div>
+        </div>
     </>
   )
 }
