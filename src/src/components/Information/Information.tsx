a896720b4de4e8dfca1073484828e60e7e299e06
tvaldig <timotiusvivaldi@gmail.com>
2023-08-07 19:08:02 +0700

    feat : add new information fixed

--- src/src/components/Information/Information.tsx
+++ src/src/components/Information/Information.tsx
@@ -11,56 +11,51 @@ class Information {
 }
   
 const DUMMY_INFORMATION: Information[] = [
-    new Information(
-      "c",
-      "August 1, 2023",
-      "info c"
-    ),
-    new Information(
-      "b",
-      "August 5, 2023",
-      "info b"
-    ),
-    new Information(
-      "Lorem Ipsum Title 3",
-      "August 10, 2023",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel odio vitae orci iaculis volutpat."
-    ),
-    new Information(
-        "Lorem Ipsum Title 1",
-        "August 1, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius turpis in lectus faucibus tincidunt."
-      ),
-      new Information(
-        "Lorem Ipsum Title 2",
-        "August 5, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo nunc ut massa tincidunt, in tristique nisi ultricies."
-      ),
-      new Information(
-        "Lorem Ipsum Title 3",
-        "August 10, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel odio vitae orci iaculis volutpat."
-      ),
-      new Information(
-        "Lorem Ipsum Title 1",
-        "August 1, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius turpis in lectus faucibus tincidunt."
-      ),
-      new Information(
-        "Lorem Ipsum Title 2",
-        "August 5, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo nunc ut massa tincidunt, in tristique nisi ultricies."
-      ),
-      new Information(
-        "Lorem Ipsum Title 3",
-        "August 10, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel odio vitae orci iaculis volutpat."
-      ),
-      new Information(
-        "Lorem Ipsum Title 3",
-        "August 10, 2023",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel odio vitae orci iaculis volutpat."
-      ),
+  new Information(
+    "Tugas habit",
+    "July 19, 2023",
+    "http://bit.ly/BentukKebiasaanBaru_"
+  ),
+  new Information(
+    "Isi pengambilan slayer",
+    "",
+    "https://docs.google.com/spreadsheets/d/1Fysca7LIT18lvGVsZtfTbLWt5qmvyr3TR5GnJxc7OZk/edit#gid=815026586"
+  ),
+  new Information(
+    "Day 3 = fun day",
+    "",
+    "https://docs.google.com/spreadsheets/d/1gJs24vuwL66e2dKndbojhhdj0ae_-sGUsFW8Df8mpnM/edit#gid=0"
+  ),
+  new Information(
+    "Kenalan spartans",
+    "Mentoring",
+    "13 orang lagi"
+  ),
+  new Information(
+    "Kenalan dengan warga HMIF",
+    "Mentoring",
+    "3 sudo dan 2 init"
+  ),
+  new Information(
+    "Wawancara BP",
+    "August 3, 2023",
+    "Pilih salah satu dari list BP"
+  ),
+  new Information(
+    "Tugas vlog with buddies",
+    "August 11, 2023",
+    "https://docs.google.com/document/d/1UwEIakYYYzcqMz3kAcdrRfOByYhWz1ySTz4kLRNuqQE/edit"
+  ),
+  new Information(
+    "Hafalin mars HMIF",
+    "",
+    "https://www.youtube.com/watch?v=he_eJki6qpM&feature=youtu.b"
+  ),
+  new Information(
+    "Milih kasuh",
+    "",
+    "di sparta22hmif.com"
+  )
   ];
   
 export { Information, DUMMY_INFORMATION };
