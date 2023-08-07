4db343d21b900cb801380a78b3cfcb2c39fc9f6a
hugosabam <hugo.sasianturi@gmail.com>
2023-08-07 13:49:33 +0700

    feat : add simple filter

--- src/src/components/HeaderUser.tsx
+++ src/src/components/HeaderUser.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { Children } from 'react';
 
-type Props = {};
 
-const HeaderUser = (props: Props) => {
+type Props = {
+  children: React.ReactNode;
+  onFilterClick1?: () => void;
+  onFilterClick2?: () => void;
+};
+
+const HeaderUser = ({children,onFilterClick1,onFilterClick2}: Props) => {
   return (
     <header className="flex items-center justify-between bg-amber-900 p-5 shadow-md mb-2 shadow-opacity-70 custom-shadow">
       <div className="rounded-full w-16 h-16 bg-orange-400"></div>
@@ -22,6 +27,10 @@ const HeaderUser = (props: Props) => {
         <Link href='/'>
           <button className='px-4 py-2 border rounded-full border-gray-400 bg-orange-400 font-bold'>Home</button>
         </Link>
+        <div className='grid'>
+          <button className='px-4 py-2 border rounded-full border-gray-400 bg-orange-400 font-bold focus:ring-2 focus:ring-red-500' onClick={onFilterClick1}>Filter Alfabet</button>
+          <button className='px-4 py-2 border rounded-full border-gray-400 bg-orange-400 font-bold focus:ring-2 focus:ring-red-500' onClick={onFilterClick2}>Filter Date</button>
+        </div>
       </div>
     </header>
   );
